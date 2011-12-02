fd9c415611281e4d236e9d053adbd709902f5ad9
root <admin@realitygroup.ru>
2011-12-02 12:52:30 +0300

    bug fixes

--- main/views/company_interface/business/interaction-index.php
+++ main/views/company_interface/business/interaction-index.php
@@ -40,7 +40,7 @@
 							<div class="left-menu">
 								<ul>
 								<?php for($i=0;$i<count($sections);$i++): ?>		
-<li><?=anchor('business-environment/interactions/'.$userinfo['uconfirmation'].'/section/'.$sections[$i]['int_id'],$sections[$i]['int_title']);?></li>
+<li><?=anchor('business-environment/interactions/'.$userinfo['uconfirmation'].'/section/'.$sections[$i]['int_id'],$sections[$i]['int_title'],array('id'=>'link'.$sections[$i]['int_id']));?></li>
 								<?php endfor; ?>
 								</ul>
 								<br />
@@ -128,6 +128,7 @@
 		$(document).ready(function(){
 			$("#lnk-logout").click(function(){$.ajax({url:"<?=$baseurl;?>shutdown",success: function(data){window.setTimeout("window.location='<?=$baseurl;?>'",1000);},error: function(){msgerror("Выход не выполнен!");}});});
 			$("#select-category").change(function(){change_category($(this));});
+			$("#link<?=$this->uri->segment(5);?>").addClass("activeTheme");
 			function change_category(obj){if(obj.val() != 'empty')window.location='<?=$baseurl;?>'+'business-environment/'+obj.val()+'/<?=$userinfo['uconfirmation'];?>';};
 		});
 </script>
